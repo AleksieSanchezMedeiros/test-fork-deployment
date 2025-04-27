ccc3cea374ac0a34b75bbfe6fbedb5523383e601
Aleksie Sanchez Medeiros <52937030+AleksieSanchezMedeiros@users.noreply.github.com>
2025-04-27 00:53:30 -0700

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   server: {
     allowedHosts: [
       '91bca94d-608c-41aa-bbe6-aea6ce74227d-00-32ysfe8xgm25g.janeway.replit.dev',
+      '3.147.71.164',
+      'ec2-3-147-71-164.us-east-2.compute.amazonaws.com',
     ],
   },
 })
