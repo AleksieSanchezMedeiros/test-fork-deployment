e9e9db12b9b24d69309f4828c4843c584f1102ba
Aleksie Sanchez Medeiros <52937030+AleksieSanchezMedeiros@users.noreply.github.com>
2025-04-27 00:52:12 -0700

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '91bca94d-608c-41aa-bbe6-aea6ce74227d-00-32ysfe8xgm25g.janeway.replit.dev',
+    ],
+  },
 })
